d01ce74eac73c926f0ecc9262d944b637e185b66
Khushal <khushal.banks@gmail.com>
2024-10-01 10:34:39 +0530

    qutebrowser, kitty & mpv player - Initial Setup

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,4 @@
+config.load_autoconfig()
+config.source('gruvbox.py')
+config.bind('<Ctrl+T>', 'spawn --userscript translate.sh en')
+config.bind('<Ctrl+Shift+T>', 'spawn --userscript translate.sh hin')
