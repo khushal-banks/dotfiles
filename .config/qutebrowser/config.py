61a6a0fd872de7af51bc276fbf3489da3734d9b6
Khushal <khushal.banks@gmail.com>
2024-10-13 22:40:46 +0530

    Another Update -- WIP

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,11 @@
 config.load_autoconfig()
+c.messages.timeout = 5000
+
 config.source('gruvbox.py')
 config.bind('<Ctrl+T>', 'spawn --userscript translate.sh en')
 config.bind('<Ctrl+Shift+T>', 'spawn --userscript translate.sh hin')
+config.bind('<Ctrl+M>', 'spawn mpv {url}')
+
+config.bind('<Ctrl+Shift+Up>', 'zoom-in')
+config.bind('<Ctrl+Shift+Down>', 'zoom-out')
+config.bind('<Ctrl+Shift+Space>', 'zoom')
